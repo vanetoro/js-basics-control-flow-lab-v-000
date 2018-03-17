7a824a3eefffb721c2b30190aa135ba1ee18be38
Vanessa Toro <vanetoro84@gmail.com>
2018-03-17 18:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Write your code in this file!
  function scuberGreetingForFeet(distance){
    if(distance < 400){
-     return "This one is on me!"
+     return "This one is on me!";
    } else 
-        if(distance > 2000)
-     
+        if(distance > 2000){
+     return "That will be $30";
    }
  }
