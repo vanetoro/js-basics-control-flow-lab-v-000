ea28cd940c5528672ded9bebb8abd2271d547490
Vanessa Toro <vanetoro84@gmail.com>
2018-03-17 18:25:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
    if(distance < 400){
      return "This one is on me!";
    } else 
-        if(distance > 2000){
+        if(distance > 2000 && distance < 2500){
       return 'I will gladly take your thirty bucks.';
-   }
+   } else
+    if(distance > 2500 ){
+      return 'No can do'
+    }
  }
